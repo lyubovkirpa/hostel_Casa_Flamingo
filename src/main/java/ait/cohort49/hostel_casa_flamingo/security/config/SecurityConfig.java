c735beaf6ed47f2dc7ee3dbc44d85641ad014fa9
yulliaivanova <gerda88823@gmail.com>
2025-01-16 23:49:07 +0100

    #14 security configuration-class

---
+++ src/main/java/ait/cohort49/hostel_casa_flamingo/security/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package ait.cohort49.hostel_casa_flamingo.security.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(ses-> ses.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers(HttpMethod.GET, "/beds").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/beds{id}").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/beds").hasRole("ADMIN")
+                );
+        return http.build();
+
+
+    }
+
+
+}
