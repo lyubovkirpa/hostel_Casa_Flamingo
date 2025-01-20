2af1233eddd32e1aee06978c6cbf45a56d93d2d2
yulliaivanova <gerda88823@gmail.com>
2025-01-20 09:46:49 +0100

    HCF-17 Create UserRepository, RoleRepository (#9)

---
+++ src/main/java/ait/cohort49/hostel_casa_flamingo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package ait.cohort49.hostel_casa_flamingo.repository;
+
+import ait.cohort49.hostel_casa_flamingo.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByFirstNameAndLastName(String firstName, String lastName);
+}
