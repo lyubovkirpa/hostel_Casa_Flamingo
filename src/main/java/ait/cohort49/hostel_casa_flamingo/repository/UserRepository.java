6d56d0065ee8b360a8913735277b4f9e53cf68e9
yulliaivanova <gerda88823@gmail.com>
2025-01-20 14:34:02 +0100

    HCF-17 Create UserRepository, RoleRepository,

--- src/main/java/ait/cohort49/hostel_casa_flamingo/repository/UserRepository.java
+++ src/main/java/ait/cohort49/hostel_casa_flamingo/repository/UserRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByFirstNameAndLastName(String firstName, String lastName);
+    Optional<User> findUserByEmail(String email);
 }
