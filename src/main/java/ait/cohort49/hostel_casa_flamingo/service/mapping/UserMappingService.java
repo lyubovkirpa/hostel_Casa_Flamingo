770fea2fc5a6493ef57f7f0b438034c020c00a22
Dell <kirpa.lyubov.a@gmail.com>
2025-01-21 10:57:03 +0100

    HCF-15 add UserMappingService.

---
+++ src/main/java/ait/cohort49/hostel_casa_flamingo/service/mapping/UserMappingService.java
@@ -0,0 +1,16 @@
+package ait.cohort49.hostel_casa_flamingo.service.mapping;
+
+import ait.cohort49.hostel_casa_flamingo.model.dto.UserDto;
+import ait.cohort49.hostel_casa_flamingo.model.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = CartMappingService.class)
+public interface UserMappingService {
+
+    @Mapping(target = "id", ignore = true)
+    User mapDtoToEntity (UserDto userDto);
+
+    UserDto mapEntityToDto(User entity);
+
+}
