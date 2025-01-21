295f134af10dc93886eb4bc8e9f5d5ff7b02bd02
crysstone <kira.ganina@outlook.com>
2025-01-21 14:35:12 +0100

    Entity room (#14)

--- src/main/java/ait/cohort49/hostel_casa_flamingo/service/interfaces/RoomService.java
+++ src/main/java/ait/cohort49/hostel_casa_flamingo/service/interfaces/RoomService.java
@@ -1,9 +1,9 @@
 package ait.cohort49.hostel_casa_flamingo.service.interfaces;
 
+import ait.cohort49.hostel_casa_flamingo.model.dto.CreateOrUpdateRoomDto;
 import ait.cohort49.hostel_casa_flamingo.model.dto.RoomDto;
 import ait.cohort49.hostel_casa_flamingo.model.entity.Room;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 public interface RoomService {
@@ -11,11 +11,10 @@ public interface RoomService {
     List<RoomDto> getAllRooms();
 
     RoomDto getRoomById(Long id);
-    RoomDto createRoom(RoomDto roomDto);
-   /* RoomDto updateRoom(Long id); */
+
+    RoomDto createRoom(CreateOrUpdateRoomDto roomDto);
+
     void  deleteRoom(Long id);
 
-//        long getRoomCount();
-//
-//        BigDecimal getTotalPrice();
+    Room findByIdOrThrow(Long id);
 }
