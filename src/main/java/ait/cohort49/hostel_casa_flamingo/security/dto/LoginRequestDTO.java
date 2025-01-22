875a90d9ba82244de34eb6ad419a891bba852b87
AIT-Reutow <andrey.reutow@ait-tr.de>
2025-01-22 21:08:29 +0000

    HCF-19 Add validation logic and tests for DTOs
    Implemented validation annotations for `RegisterRequestDTO` and `LoginRequestDTO`, ensuring fields meet specified requirements. Added comprehensive unit tests to verify validation behavior, including positive and negative cases for passwords, emails, and other fields. Introduced `ValidationTestUtil` to streamline validation testing logic.

--- src/main/java/ait/cohort49/hostel_casa_flamingo/security/dto/LoginRequestDTO.java
+++ src/main/java/ait/cohort49/hostel_casa_flamingo/security/dto/LoginRequestDTO.java
@@ -1,4 +1,13 @@
 package ait.cohort49.hostel_casa_flamingo.security.dto;
 
-public record LoginRequestDTO(String userEmail, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDTO(@NotBlank(message = "{validation.email.not_blank}")
+                              @Email(message = "{validation.email.invalid}")
+                              String userEmail,
+
+                              @NotBlank(message = "{validation.password.not_blank}")
+                              String password
+) {
 }
--- src/main/java/ait/cohort49/hostel_casa_flamingo/security/dto/RegisterRequestDTO.java
+++ src/main/java/ait/cohort49/hostel_casa_flamingo/security/dto/RegisterRequestDTO.java
@@ -5,14 +5,25 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-public record RegisterRequestDTO(@NotBlank
-                                 @Email
-                                 String userEmail,
-                                 @NotBlank
-                                 @Pattern(regexp = ValidationConstants.PASSWORD_REGEX)
-                                 String password,
-                                 String phoneNumber,
-                                 String firstName,
-                                 String lastName) {
 
+public record RegisterRequestDTO(@NotBlank(message = "{validation.email.not_blank}")
+                                 @Email(message = "{validation.email.invalid}")
+                                 String userEmail,
+
+                                 @NotBlank(message = "{validation.password.not_blank}")
+                                 @Pattern(
+                                         regexp = ValidationConstants.PASSWORD_REGEX,
+                                         message = "{validation.password.invalid}"
+                                 )
+                                 String password,
+
+                                 @NotBlank(message = "{validation.phone_number.not_blank}")
+                                 String phoneNumber,
+
+                                 @NotBlank(message = "{validation.first_name.not_blank}")
+                                 String firstName,
+
+                                 @NotBlank(message = "{validation.last_name.not_blank}")
+                                 String lastName
+) {
 }
