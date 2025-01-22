a50cd7a86be93fa898165e56b22dd1388a6e2f87
AIT-Reutow <andrey.reutow@ait-tr.de>
2025-01-22 21:18:48 +0000

    HCF-19 Add @PreAuthorize import to BedController
    The `@PreAuthorize` annotation is now supported in `BedController` by adding the necessary import. This prepares the groundwork for implementing method-level security. No functional changes are introduced yet.

--- src/main/java/ait/cohort49/hostel_casa_flamingo/controller/BedController.java
+++ src/main/java/ait/cohort49/hostel_casa_flamingo/controller/BedController.java
@@ -4,6 +4,7 @@ package ait.cohort49.hostel_casa_flamingo.controller;
 import ait.cohort49.hostel_casa_flamingo.model.dto.BedDto;
 import ait.cohort49.hostel_casa_flamingo.service.BedCreateDto;
 import ait.cohort49.hostel_casa_flamingo.service.interfaces.BedService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
