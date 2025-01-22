8b5de128fe264f805247047d7ddde3fd8e92f859
AIT-Reutow <andrey.reutow@ait-tr.de>
2025-01-22 21:07:23 +0000

    HCF-19 Refactor RoleDto and implement RoleService with mapping.
    Replaced RoleDto class with a record to simplify data structure. Added RoleService interface and its implementation to handle role retrieval by title with exception handling. Updated RoleMappingService to map Role entity to RoleDto with the new name/title correspondence.

--- src/main/java/ait/cohort49/hostel_casa_flamingo/model/dto/RoleDto.java
+++ src/main/java/ait/cohort49/hostel_casa_flamingo/model/dto/RoleDto.java
@@ -1,44 +1,5 @@
 package ait.cohort49.hostel_casa_flamingo.model.dto;
 
-import java.util.Objects;
+public record RoleDto(String name) {
 
-public class RoleDto {
-
-    private Long id;
-    private String title;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        RoleDto roleDto = (RoleDto) o;
-        return Objects.equals(id, roleDto.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Role: id - %d, name - %s", id, title);
-    }
 }
